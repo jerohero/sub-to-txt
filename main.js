ef553116374aec841633469b2daf5a66523552a6
Jeroen <jegbol01@gmail.com>
2020-04-21 23:19:16 +0200

    red me

--- main.js
+++ main.js
@@ -9,43 +9,62 @@ function start(pressedBtn){
         reader.onload = function(e){
             if(title.endsWith(".srt")){
                 var rawsub = e.target.result;
-                converted = convert(rawsub);
+                converted = convert(rawsub, pressedBtn, title);
+                document.getElementsByClassName("hiddenOutput")[0].innerText = converted;
                 if(pressedBtn == "download"){
-                    converted = addTitle(converted, title);
                     download(converted, "converted");
                 }
                 else if(pressedBtn == "show"){
-                    show(converted, title)
+                    show(converted, title);
         }   }   }
     }   
 }
 
-function convert(rawsub){
+function convert(rawsub, pressedBtn, title){
     let lines = rawsub.split("\n");
     var text = "";
+    if(pressedBtn == "download"){
+        title = title.replace(".srt", "");
+        text = title + "\n" + "—".repeat(title.length) + "\n";
+    }
 
     for (let index = 0; index < lines.length; index++) {
         let line = lines[index];
-        if(line.includes("<i>" || line.includes("</i>"))) line = line.replace("<i>", "");
-        if(line.includes("</i>")) line = line.replace("</i>", "");
-        if( line.includes("-->") || parseInt(line)){
-            continue
+
+        if(parseInt(line)){continue}
+        else if( line.length <= 1){continue}
+        else if( line.includes("-->")) {
+            if(pressedBtn == "show"){
+                var time = line.substring(0, line.indexOf(","));
+                var htmlTime = "<span id='time'>" + time + "</span>"; 
+            }continue}
+        else{
+            if(pressedBtn == "show"){
+                text = text + htmlTime + line + "<br>";
+            }
+            else if(pressedBtn == "download"){
+                if(line.includes("<i>") || line.includes("</i>")) line = line.replace("<i>", "");
+                if(line.includes("</i>")) line = line.replace("</i>", "");
+                text = text + line + "\n\n";
+            }
         }
-        text = text + line + "\n";
     }
     return text;
 }
 
-function addTitle(text, title){
-    title = title.replace(".srt", "");
-    text = title + "\n" + "—".repeat(title.length) + "\n" + text;
-    return text;
-}
-
 function show(data, title){
-    document.getElementById("textArea").className = "active";
-    document.getElementById("title").innerText = title;
-    document.getElementById("text").innerText = data;
+    var titleElement = document.getElementsByClassName("title")[0];
+    var textArea = document.getElementsByClassName("textArea")[0];
+    title = title.replace(".srt", "");
+    titleElement.innerText = title;
+
+    // textArea.className = textArea.className.replace("disabled", "active");
+    textArea.className = "textArea enabled";
+    // document.getElementById("text").innerText = data;
+    document.getElementsByClassName("text")[0].innerHTML = data;
+
+
+    document.getElementsByClassName("textArea")[0].scrollIntoView({behavior: "smooth"});
 }
 
 function download(data, filename){
@@ -65,3 +84,21 @@ function download(data, filename){
         }, 0);
     }
 }
+
+
+var copyBtn = document.getElementById("copyBtn");
+var clipboard = new ClipboardJS(copyBtn);
+
+clipboard.on('success', function(e){
+    console.log(e);
+});
+clipboard.on('error', function(e){
+    console.log(e);
+});
+
+// var textToCopy = document.getElementsByClassName("hiddenOutput")[0];
+// var textToCopy = document.getElementsByClassName("textArea")[0];
+// textToCopy.select();
+// // textToCopy.setSelectionRange(0, 99999); //for mobile devices
+// // document.execCommand("copy");
+// document.execCommand("copy");
